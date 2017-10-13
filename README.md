f7c2a18646a1a8a2bf49fb03d101f563c905cfad
沈鸿飞 <shf19961008@163.com>
2017-10-13 23:22:01 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,84 @@
+# 大工软院 寝室免费高速IPv6网络 配置指南
+
+A configuration guide for SSDUT(Software School of Dalian University of Technology) students to surf the internet freely in their dormitories.
+
+本文链接
+
+
+
+## 功能
+
+1.  无需登录计费系统，不消耗寝室校园网流量。
+
+    ![no-login](img/no-login.png)
+
+2.  充分利用高速IPv6网络，下载速度平均 `20 MB/s` ，最高可达 `50MB/s` ，延迟 `170ms` 左右。
+
+    ![speed-idm](img/speed-idm.png)
+
+    ![speed-shadowsocks](img/speed-shadowsocks.png)
+
+3.  自由浏览国外网站 Google, Youtube。玩外服游戏... (理论上访问国外网站速度会更快）
+
+## 使用说明书
+
+1.  疏通IPv6网络
+
+    1.  在开始菜单中找到 `Windows PowerShell` ，右键以管理员身份运行。
+    2.  粘贴运行以下内容。
+
+    ```powershell
+    # 配置防火墙
+        New-NetFirewallRule -DisplayName "Block IPv6 Router Advertisement" -Protocol ICMPv6 -IcmpType 134 -Action Block
+
+    # 配置路由
+        New-NetRoute -AddressFamily IPv6 -DestinationPrefix "::/0" -InterfaceAlias 以太网 -NextHop "fe80::2a0:a50f:fc7d:bf00" -RouteMetric 0 -Confirm -ErrorAction Ignore
+        New-NetRoute -AddressFamily IPv6 -DestinationPrefix "::/0" -InterfaceAlias 以太网 -NextHop "fe80::2a0:a50f:fc7d:bf01" -RouteMetric 0 -Confirm -ErrorAction Ignore
+        Get-NetRoute -AddressFamily IPv6 -InterfaceAlias 以太网 -DestinationPrefix "::/0"
+    ```
+
+    3.  测试IPv6网络是否畅通，将校园网注销，在浏览器地址栏中输入 `[2600::]` （包括方括号），应该可以打开网页。至此，**已经可以免流访问所有IPv6网站了**。
+
+    ​
+
+2.  用Shadowsocks作为代理, 通过 `能够访问IPv6网络的服务器` 转发 IPv4流量 
+
+    （服务器我已经搭好，目前开放账号供大家使用，请勿使用BT等P2P手段下载美国有版权的电影，可能会导致服务器被封，可以用百度云离线之后在下载，HTTP不受限制。）
+
+    1.  下载 
+
+    2.  启动 `Shadowsocks.exe` , 在托盘中找到小飞机图标，右键→启动系统代理。
+
+    3.  在托盘中找到小飞机图标，右键→帮助→勾选详细记录日志→显示日志。
+
+    4.  如果 看到日志中有 `Socket connected to ss server: [2607:8700:101:3118::]:666` ，则转发流量成功。
+
+    5.  将软件的本地代理设置为：协议 `Socks5` ，地址 `127.0.0.1` ，端口 `1080` 。
+
+        1.  QQ
+
+            ![qq-proxy](img/qq-proxy.png)
+
+        2.  安装浏览器扩展，参考 [http://wxhp.org/shadowsocksr.html](http://wxhp.org/shadowsocksr.html)
+
+    6.  可以愉快地玩耍了。
+
+3.  *进阶设定
+
+    1.  可以使用 `Proxifier` 让不支持Socks5代理的软件也能上网。
+
+## 原理
+
+请在上方Star
+
+![stars](img/stars.png)
+
+如果Stars多的话会更新。
+
+
+
+## 作者
+
+网安1504 沈鸿飞
+
+有问题联系 QQ:350986489
---
+++ network.ps1
@@ -0,0 +1,87 @@
+﻿# 防火墙规则
+    New-NetFirewallRule -DisplayName "Block IPv6 Router Advertisement" -Protocol ICMPv6 -IcmpType 134 -Action Block
+    Get-NetFirewallRule -DisplayName "Block IPv6 Router Advertisement"
+    Remove-NetFirewallRule -DisplayName "RA Block"
+
+
+# 路由
+    New-NetRoute -AddressFamily IPv6 -DestinationPrefix "::/0" -InterfaceAlias 以太网 -NextHop "fe80::2a0:a50f:fc7d:bf00" -RouteMetric 0 -Confirm -ErrorAction Ignore
+    New-NetRoute -AddressFamily IPv6 -DestinationPrefix "::/0" -InterfaceAlias 以太网 -NextHop "fe80::2a0:a50f:fc7d:bf01" -RouteMetric 0 -Confirm -ErrorAction Ignore
+    Get-NetRoute -AddressFamily IPv6 -InterfaceAlias 以太网 -DestinationPrefix "::/0"
+    Remove-NetRoute -AddressFamily IPv6 -DestinationPrefix "::/0" -Confirm:$false
+    
+# IP地址
+    Get-NetIPAddress -InterfaceAlias 以太网|fl *
+    Get-NetIPAddress -InterfaceAlias 以太网
+
+    Remove-NetIPAddress "2001:da8:a800:af00::49:1711" -Confirm
+    New-NetIPAddress -AddressFamily IPv6 -Type Unicast -InterfaceIndex 17 "2001:da8:a800:af00::3b:e08a"
+
+    $ethernet_ip_conf=Get-NetIPConfiguration -Detailed -InterfaceAlias 以太网
+    $ethernet_ip_conf.IPv6Address
+
+# DHCP 释放、刷新
+    Start-Process ipconfig  -ArgumentList "/release6","以太网" -Wait -WindowStyle Hidden
+    Start-Process ipconfig  -ArgumentList "/release","以太网"  -Wait -WindowStyle Hidden
+    Start-Process ipconfig  -ArgumentList "/renew","以太网"    -Wait -WindowStyle Hidden
+    Start-Process ipconfig  -ArgumentList "/renew6","以太网"   -Wait -WindowStyle Hidden
+
+    Get-Process -Name ipconfig|Select-Object {$_.Kill()}
+
+# 接口、适配器
+    $ethernet=Get-NetAdapter -Name 以太网
+    Get-NetAdapter -Name 以太网 |fl *
+    $ethernet|fl -Property Name,LinkSpeed,MediaConnectionState
+    $ether_adconf=Get-CimClass -Class Win32_NetworkAdapterConfiguration |? Index -eq 5
+
+
+# 当前是否为寝室连接
+    $default_ipv4_gw=Get-NetRoute -AddressFamily IPv4 -DestinationPrefix "0.0.0.0/0"|% NextHop
+    $default_ipv4_gw -eq "192.168.35.1"
+
+# 测试IPv6连接
+    nslookup.exe /?
+    nslookup.exe test-ipv6.comq
+
+# 
+    $time_out_cnt=0
+    while($true){
+        $ping_result=Get-WmiObject -Query "select * from win32_pingstatus where Address='2607:8700:101:3118::' and Timeout=1000" -ErrorAction SilentlyContinue -ErrorVariable ping_error
+        # $ping_error[0].CategoryInfo.Category.ToString() -eq "InvalidOperation"
+        if($ping_error){
+            Write-Host "ping error"
+            New-NetRoute -AddressFamily IPv6 -DestinationPrefix "::/0" -InterfaceAlias 以太网 -NextHop "fe80::2a0:a50f:fc7d:bf00" -RouteMetric 0 -Confirm -ErrorAction Ignore
+            New-NetRoute -AddressFamily IPv6 -DestinationPrefix "::/0" -InterfaceAlias 以太网 -NextHop "fe80::2a0:a50f:fc7d:bf01" -RouteMetric 0 -Confirm -ErrorAction Ignore
+            Get-NetRoute -AddressFamily IPv6 -InterfaceAlias 以太网 -DestinationPrefix "::/0"
+            continue
+        }
+        switch($ping_result.statuscode){
+            0 {
+                $time_out_cnt=0
+                Start-Sleep -Seconds 2
+            }
+            11010 {
+                $time_out_cnt++
+                if($time_out_cnt -eq 3){
+                    Write-Host "连接超时"
+                    # 检查以太网是否为寝室IPv6
+                    $default_ipv4_gw=Get-NetRoute -AddressFamily IPv4 -DestinationPrefix "0.0.0.0/0"|% NextHop
+                    if($default_ipv4_gw -ne "192.168.35.1"){
+
+                    }
+                    Start-Process ipconfig -WindowStyle Hidden -ArgumentList "/renew6","以太网"
+                    Start-Sleep -Seconds 5
+                    Stop-Process -Name ipconfig -ErrorAction SilentlyContinue
+                    $time_out_cnt=0
+                }else{
+                    Start-Sleep -Milliseconds 500
+                }
+            }
+            11003 {
+                Write-Host "目标主机不可达"
+            }
+            default {
+                Write-Host $ping_result.statuscode
+            }
+        }
+    }
